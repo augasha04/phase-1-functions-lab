f4ae24553187b14edda7d6739cb59f32ff70e5f3
Augustine Mwangi Kariuki <augustinemwangi04@gmail.com>
2023-03-20 14:13:05 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street){
+    distanceFromHqInBlocks =Math.abs(street - 42);
+    return distanceFromHqInBlocks;
+}
+function distanceFromHqInFeet(street){
+    distanceFromHqInFeet= distanceFromHqInBlocks * 264;
+    return distanceFromHqInFeet;
+}
+function distanceTravelledInFeet(start,destination){
+    distanceTravelledInFeet= Math.abs(destination-start) * 264;
+    return distanceTravelledInFeet;
+}
+function calculatesFarePrice(start,destination){
+    distanceTravelledInFeet= Math.abs(destination-start);
+    if (distanceTravelledInFeet <= 400) {
+        distanceTravelledInFeet  = 0;
+        return distanceTravelledInFeet;
+    } else if (distanceTravelledInFeet > 400 && distanceTravelledInFeet < 2000) {
+        distanceTravelledInFeet  = (distanceTravelledInFeet - 400) * 2;
+        return distanceTravelledInFeet ;
+    }else if (distanceTravelledInFeet > 2000 && distanceFromHqInFeet < 2500) {
+       return distanceTravelledInFeet = 25;
+    } else {
+        return distanceTravelledInFeet  = "cannot travel that far";
+    }
+  }
+
